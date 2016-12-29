bf938a1cd57c2e48ca1f6bf30f73aaa84a019b78
Diego Camere <dcamere@gmail.com>
2016-12-29 00:46:53 -0500

    Arreglado el lapsus de Javier

--- index.php
+++ index.php
@@ -55,7 +55,7 @@ get_header(); ?>
                     </div>
                     <div class="personaje">
                         <h3 class="danger"><?php the_field("character_name2"); ?></h3>
-                        <img src="<?php the_field("character_name2"); ?>" alt="">
+                        <img src="<?php the_field("character_image2"); ?>" alt="">
                         <div style="width:75%; text-align:center;margin-left:auto;margin-right:auto;"> 
                             <p class="kings"><?php the_field("character_description2"); ?> </p>
                         </div>
@@ -77,7 +77,7 @@ get_header(); ?>
                         </div>
                     </div>
                     <div class="personaje">
-                        <h3 class="danger"><?php the_field("character_image5"); ?></h3>
+                        <h3 class="danger"><?php the_field("character_name5"); ?></h3>
                         <img src="<?php the_field("character_image5"); ?>" alt="">
                         <div style="width:75%; text-align:center;margin-left:auto;margin-right:auto;"> 
                             <p class="kings"><?php the_field("character_description5"); ?></p>
